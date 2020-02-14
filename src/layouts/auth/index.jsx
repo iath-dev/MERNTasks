fbe3f64222eb7095b68fa4019160a161562473d4
iath-dev <developer.iath@gmail.com>
2020-02-13 22:31:29 -0500

    189. Creando el Formulario del Login

--- src/layouts/auth/index.jsx
+++ src/layouts/auth/index.jsx
@@ -2,7 +2,47 @@
 import React from 'react';
 
 const AuthPage = () => {
-    return ( <h1>Login</h1> );
+
+    const handleChange = (event) => {
+
+    }
+
+    return ( 
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Contraseña</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            value="Iniciar Sesión"
+                            className="btn btn-primario btn-block"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+     );
 }
  
 export default AuthPage;
